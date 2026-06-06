4ae2f00d9506b06bca59a69923d273de54a04f2e
sr931662 <83828392+sr931662@users.noreply.github.com>
2026-06-06 17:19:15 +0530

    Fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+        },
       },
     },
-    middlewareMode: true,
-  },
 
   build: {
     // Optimize build output
